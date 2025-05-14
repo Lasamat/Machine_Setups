f5bac6b166175d45fe0d88b9be34b662ae85ed4d
Lasamat <lasamat@Lasamat-PC.>
2025-05-14 09:26:16 +0200

    Add zsh and oh-my-zsh to Linux automate

--- Linux/automate.sh
+++ Linux/automate.sh
@@ -1,2 +1,11 @@
 !#/bin/bash
+
 sudo apt install zsh
+# Change default shell from Bash to ZSH
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "ZSH_THEME=\"agnoster\"" >.zshrc
+echo "source $ZSH/oh-my-zsh.sh" >.zshrc
